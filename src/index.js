ba4944f54d50c9c2ed97bb4de5295bf15eda5fc3
Graham Munro <graham.munro@thoughtworks.com>
2019-02-11 12:55:26 -0700

    Github test

--- src/index.js
+++ src/index.js
@@ -15,11 +15,7 @@ const router = (
   </Provider>
 )
 
-
+// hello
 
 ReactDOM.render(router, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
